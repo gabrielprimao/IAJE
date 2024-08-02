be9d269729dc1a01d3656fcd006a9216edaa65d3
gabrielprimao <110679259+gabrielprimao@users.noreply.github.com>
2024-08-02 09:27:02 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,14 @@
+const caixaPrincipal = document.querySelector('.caixa-principal');
+const caixaPergunta = document.querySelector(".caixa-pergunta");
+const caixaAlternativa = document.querySelector(".caixa-alternaiva");
+const caixaResultado = document.querySelector('.caixa-resutado');
+
+const perguntas = [//abre lista de objetos (itens)
+    {//abre o item
+    enunciado:"pergunta 1"
+    alternativas:[
+        "alternativa 1"
+        "alternativa 2"
+    ]
+    }
+]
